ca4667c14a6cde8b6e87ea7d65938e50d96b6d00
Joooban <jvanmorden@gmail.com>
2024-09-08 18:16:45 +0800

    Uploading Tokenizer

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Tokenizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblTextInput = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnTokenize = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbDelimeter = new System.Windows.Forms.ComboBox();
+            this.dgvOutput = new System.Windows.Forms.DataGridView();
+            this.clmTok = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmCompo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTextInput
+            // 
+            this.lblTextInput.AutoSize = true;
+            this.lblTextInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTextInput.Location = new System.Drawing.Point(12, 50);
+            this.lblTextInput.Name = "lblTextInput";
+            this.lblTextInput.Size = new System.Drawing.Size(86, 20);
+            this.lblTextInput.TabIndex = 0;
+            this.lblTextInput.Text = "Enter Text:";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(172, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(231, 25);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "STRING TOKENIZER";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(16, 73);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(235, 150);
+            this.txtInput.TabIndex = 3;
+            // 
+            // btnTokenize
+            // 
+            this.btnTokenize.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTokenize.Location = new System.Drawing.Point(23, 310);
+            this.btnTokenize.Name = "btnTokenize";
+            this.btnTokenize.Size = new System.Drawing.Size(217, 30);
+            this.btnTokenize.TabIndex = 6;
+            this.btnTokenize.Text = "Tokenize";
+            this.btnTokenize.UseVisualStyleBackColor = true;
+            this.btnTokenize.Click += new System.EventHandler(this.btnTokenize_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 244);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Delimeter";
+            // 
+            // cbDelimeter
+            // 
+            this.cbDelimeter.FormattingEnabled = true;
+            this.cbDelimeter.Location = new System.Drawing.Point(17, 267);
+            this.cbDelimeter.Name = "cbDelimeter";
+            this.cbDelimeter.Size = new System.Drawing.Size(234, 21);
+            this.cbDelimeter.TabIndex = 8;
+            // 
+            // dgvOutput
+            // 
+            this.dgvOutput.AllowUserToAddRows = false;
+            this.dgvOutput.AllowUserToDeleteRows = false;
+            this.dgvOutput.AllowUserToOrderColumns = true;
+            this.dgvOutput.AllowUserToResizeColumns = false;
+            this.dgvOutput.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvOutput.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOutput.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmTok,
+            this.clmType,
+            this.clmCompo});
+            this.dgvOutput.Location = new System.Drawing.Point(311, 50);
+            this.dgvOutput.Name = "dgvOutput";
+            this.dgvOutput.ReadOnly = true;
+            this.dgvOutput.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgvOutput.Size = new System.Drawing.Size(417, 290);
+            this.dgvOutput.TabIndex = 9;
+            // 
+            // clmTok
+            // 
+            this.clmTok.HeaderText = "Token";
+            this.clmTok.Name = "clmTok";
+            this.clmTok.ReadOnly = true;
+            this.clmTok.Width = 125;
+            // 
+            // clmType
+            // 
+            this.clmType.HeaderText = "Type";
+            this.clmType.Name = "clmType";
+            this.clmType.ReadOnly = true;
+            this.clmType.Width = 125;
+            // 
+            // clmCompo
+            // 
+            this.clmCompo.HeaderText = "Components";
+            this.clmCompo.Name = "clmCompo";
+            this.clmCompo.ReadOnly = true;
+            this.clmCompo.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 363);
+            this.Controls.Add(this.dgvOutput);
+            this.Controls.Add(this.cbDelimeter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnTokenize);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lblTextInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tokenizer";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTextInput;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnTokenize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbDelimeter;
+        private System.Windows.Forms.DataGridView dgvOutput;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmTok;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmType;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmCompo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tokenizer
+{
+    public partial class Form1 : Form
+    {
+        private readonly Dictionary<string, char> predefinedDelimiters = new Dictionary<string, char>
+        {
+            { "Space", ' ' },
+            { "Comma", ',' },
+            { "Period", '.' },
+            { "Exclamation", '!' },
+            { "Question Mark", '?' },
+            { "Hyphen", '-' },
+            { "Slash", '/' }
+        };
+        public Form1()
+        {
+            InitializeComponent();
+            foreach (var delimiter in predefinedDelimiters.Keys)
+            {
+                cbDelimeter.Items.Add(delimiter);
+            }
+            cbDelimeter.SelectedIndex = 0; // para si space ang una magpakita instead of blank
+        }
+
+        private List<string> Tokenize(string text, char[] delimiters)
+        {
+            List<string> tokens = new List<string>();
+            StringBuilder currentToken = new StringBuilder();
+            bool isBuildingNumber = false;  // Flag to indicate if currently building a number token
+            bool hasDecimalPoint = false;   // Flag to track if a decimal point has been encountered in a number
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char ch = text[i];
+
+                // Check if character is a delimiter
+                if (delimiters.Contains(ch))
+                {
+                    if (currentToken.Length > 0)
+                    {
+                        tokens.Add(currentToken.ToString());
+                        currentToken.Clear();
+                        isBuildingNumber = false;
+                        hasDecimalPoint = false;
+                    }
+                }
+                // Check if character is part of a number (digits and at most one decimal point)
+                else if (char.IsDigit(ch))
+                {
+                    currentToken.Append(ch);
+                    isBuildingNumber = true;
+                }
+                else if (ch == '.' && isBuildingNumber && !hasDecimalPoint && i + 1 < text.Length && char.IsDigit(text[i + 1]))
+                {
+                    // Only treat the '.' as part of the number if it's followed by another digit
+                    hasDecimalPoint = true;
+                    currentToken.Append(ch);
+                }
+                // Check if token is alphanumeric (contains both letters and digits)
+                else if (char.IsLetterOrDigit(ch))
+                {
+                    currentToken.Append(ch);
+                }
+                // Check if character is punctuation and should be separated
+                else if (char.IsPunctuation(ch))
+                {
+                    if (currentToken.Length > 0)
+                    {
+                        tokens.Add(currentToken.ToString());
+                        currentToken.Clear();
+                        isBuildingNumber = false;
+                        hasDecimalPoint = false;
+                    }
+
+                    tokens.Add(ch.ToString());  // Add punctuation as a separate token
+                }
+                else if (ch == '\r' && i + 1 < text.Length && text[i + 1] == '\n')  // Windows new line
+                {
+                    if (currentToken.Length > 0)
+                    {
+                        tokens.Add(currentToken.ToString());
+                        currentToken.Clear();
+                    }
+                    tokens.Add("\n");  // Add "\n" as the token for the end of line
+                    i++; // Skip the next '\n' to prevent double-counting
+                    isBuildingNumber = false;
+                    hasDecimalPoint = false;
+                }
+                else if (ch == '\n')  // Unix/MacOS new line
+                {
+                    if (currentToken.Length > 0)
+                    {
+                        tokens.Add(currentToken.ToString());
+                        currentToken.Clear();
+                    }
+                    tokens.Add("\n");  // Add "\n" as the token for the end of line
+                    isBuildingNumber = false;
+                    hasDecimalPoint = false;
+                }
+                else
+                {
+                    // If we were building a number but hit a non-number character, finalize the current token
+                    if (isBuildingNumber)
+                    {
+                        tokens.Add(currentToken.ToString());
+                        currentToken.Clear();
+                        isBuildingNumber = false;
+                        hasDecimalPoint = false;
+                    }
+
+                    currentToken.Append(ch);
+                }
+            }
+
+            // Add the last token if it's not empty
+            if (currentToken.Length > 0)
+            {
+                tokens.Add(currentToken.ToString());
+            }
+
+            return tokens;
+        }
+
+
+
+
+        private string ClassifyToken(string token, char selectedDelimiter)
+        {
+            // Remove leading and trailing spaces from the token
+            token = token.Trim();
+
+            // Check for specific token types
+            if (string.IsNullOrWhiteSpace(token))
+                return "End of Line";
+
+            if (token == "\n")
+                return "End of Line";
+
+            // Check if token is a single punctuation character from predefined delimiters
+            if (token.Length == 1 && predefinedDelimiters.Values.Contains(token[0]))
+                return "Punctuation";
+
+            // Check if token consists only of letters
+            if (token.All(char.IsLetter))
+                return "Word";
+
+            // Check if token consists only of digits
+            if (token.All(char.IsDigit))
+                return "Number";
+
+            // Check if token contains both letters and digits (alphanumeric)
+            if (token.Any(char.IsLetter) && token.Any(char.IsDigit))
+                return "Alphanumeric";
+
+            // Check if token is a valid number format (handles decimals)
+            if (token.Contains('.') && token.Count(char.IsDigit) > 0 && token.Count(c => c == '.') <= 1)
+                return "Number";
+
+            char[] symbolsToExclude = new char[] { '@', '#', '$', '%', '&', '*', '(', ')' };
+            // Check if the token consists of punctuation ug dili kana sa taas
+            if (token.All(c => char.IsPunctuation(c) && !symbolsToExclude.Contains(c)))
+                return "Punctuation";
+
+            // Default case for special characters
+            return "Special Characters";
+        }
+
+
+        private List<(string, string, List<char>)> BreakdownTokens(List<string> tokens, char selectedDelimiter)
+        {
+            // Ga store daw siya ug duha ka string then list of char, which idk unsa hahaha
+            // Good way to visualize : First Name, Family Name, Gender (M/F)
+            List<(string, string, List<char>)> breakdown = new List<(string, string, List<char>)>();
+
+            foreach (var token in tokens)
+            {
+                string tokenType = ClassifyToken(token, selectedDelimiter); // Pass the delimiter to classify
+
+                // If the token is "\n", add no components
+                if (token == "\n")
+                    breakdown.Add((token, tokenType, new List<char>()));
+                else
+                    breakdown.Add((token, tokenType, token.ToList()));
+            }
+
+            return breakdown;
+        }
+
+        private void btnTokenize_Click(object sender, EventArgs e)
+        {
+            string text = txtInput.Text;
+
+            // Get the selected delimiter from the ComboBox
+            char selectedDelimiter = predefinedDelimiters[cbDelimeter.SelectedItem.ToString()];
+
+            // Use the selected delimiter as a single-element array
+            char[] delimiters = new char[] { selectedDelimiter };
+
+            List<string> tokens = Tokenize(text, delimiters);
+            var granularTokens = BreakdownTokens(tokens, selectedDelimiter);
+
+            // Clear the DataGridView before displaying new results
+            dgvOutput.Rows.Clear();
+
+            // Responsible sa pag display
+            foreach (var (token, tokenType, components) in granularTokens)
+            {
+                dgvOutput.Rows.Add(token, tokenType, string.Join(", ", components));
+            }
+
+            txtInput.Text = "";
+        }
+    }
+}
